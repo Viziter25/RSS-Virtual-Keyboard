f4e8602abe6758fbb306e0409719b5897c6254af
Illia <viziter.1994@mail.ru>
2022-05-10 00:58:43 +0300

    feat: add keyboar on ru lang

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 let keyboardEng = [ 96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8, '', 9, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 'del', '', 20, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 13, '', 16, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 38, 16, '', 17, 'win', 18, 32, 18, 37, 40, 'right', 17];
 
-let keyboardRu = [ 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8, '', 9, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 'del', '', 20, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 13, '', 16, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 38, 16, '', 17, 'win', 18, 32, 18, 37, 40, 'right', 17]
+let keyboardRu = [ 96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8, '', 9, 1081, 1094, 1091, 1082, 1077, 1085, 1075, 1096, 1097, 1079, 1093, 1098, 92, 'del', '', 20, 1092, 1099, 1074, 1072, 1087, 1088, 1086, 1083, 1076, 1078, 1101, 13, '', 16, 1103, 1095, 1089, 1084, 1080, 1090, 1100, 1073, 1102, 46, 38, 16, '', 17, 'win', 18, 32, 18, 37, 40, 'right', 17]
 // let arr = [];
 // document.onkeypress = (event => {
 //     arr.push(event.charCode);
@@ -21,7 +21,12 @@ const div = document.createElement('div');
 div.id ='keyboard';
 container.append(div);
 
-
+const p = document.createElement('p');
+const p1 = document.createElement('p');
+p.innerHTML = 'Клавиатура создана в операционной системе Windows';
+p1.innerHTML = 'Для переключения языка комбинация: левыe ctrl + alt';
+container.append(p);
+container.append(p1);
 
 const input = document.querySelector('.input');
 function init(keyboard) {
@@ -44,7 +49,7 @@ function init(keyboard) {
         } else if (keyboard[i] == '16') {
             out += '<div class = "shift" data = "'+ keyboard[i] +'" id = "shift">' + 'Shift' + '</div>';
         } else if (keyboard[i] == '38') {
-            out += '<div class = "up" data = "'+ keyboard[i] +'" id = "up">' + '<p>^</p>' + '</div>';
+            out += '<div class = "up" data = "'+ keyboard[i] +'" id = "up">' + '^' + '</div>';
         } else if (keyboard[i] == '17') {
             out += '<div class = "ctrl" data = "'+ keyboard[i] +'" id = "ctrl">' + 'Ctrl' + '</div>';
         } else if (keyboard[i] == 'win') {
@@ -52,11 +57,11 @@ function init(keyboard) {
         } else if (keyboard[i] == '18') {
             out += '<div class = "alt" data = "'+ keyboard[i] +'" id = "alt">' + 'Alt' + '</div>';
         } else if (keyboard[i] == '37') {
-            out += '<div class = "left" data = "'+ keyboard[i] +'" id = "left">' + '<p><</p>' + '</div>';
+            out += '<div class = "left" data = "'+ keyboard[i] +'" id = "left">' + '<' + '</div>';
         } else if (keyboard[i] == '40') {
-            out += '<div class = "down" data = "40" id = "down"' + 'down' + '">' + '<p>^</p>' + '</div>';
+            out += '<div class = "down" data = "'+ keyboard[i] +'" id = "down">' + '^' + '</div>';
         } else if (keyboard[i] == 'right') {
-            out += '<div class = "right" data = "39" id = "right"' + 'right' + '">' + '<p>></p>' + '</div>';
+            out += '<div class = "right" data = "'+ keyboard[i] +'" id = "right">' + '>' + '</div>';
         } else {
             out += '<div class = "k-key" data = "' + keyboard[i] + '">' + String.fromCharCode(keyboard[i]) + '</div>';
         } 
@@ -70,13 +75,19 @@ init(keyboardEng);
 let langEn = true;
 let shiftPressed = false;
 const buttons = document.querySelectorAll('.k-key');
-const shift = document.querySelector('#shift');
+const shift = document.querySelectorAll('#shift');
 const up = document.querySelector('#up');
+const left = document.querySelector('#left');
+const right = document.querySelector('#right');
+const down = document.querySelector('#down');
 const del = document.querySelector('#del');
 const capsLock = document.querySelector('#caps_lock');
 const tab = document.querySelector('#tab');
 const space = document.querySelector('#space');
 const enter = document.querySelector('#enter');
+const ctrl = document.querySelectorAll('#ctrl');
+const alt = document.querySelectorAll('#alt');
+const win = document.querySelector('#win');
 
 
 
@@ -125,9 +136,19 @@ enter.addEventListener('click', () => {
 });
 
 
-//up
+//arrows
 up.addEventListener('click', () => {
     input.value = input.value + up.innerHTML;
+});
+left.addEventListener('click', () => {
+    console.log(left.innerHTML);
+    input.value = input.value + left.innerHTML;
+});
+right.addEventListener('click', () => {
+    input.value = input.value + right.innerHTML;
+});
+down.addEventListener('click', () => {
+    input.value = input.value + down.innerHTML;
 });
 
 
@@ -137,11 +158,11 @@ del.addEventListener('click', () => {
 });
 
 
-// del
-shift.addEventListener('click',() => {
-    shift.classList.toggle('active');
+// shift
+// shift.addEventListener('click',() => {
+//     shift.classList.toggle('active');
     
-});
+// });
 
 
 document.onkeypress = function (event) {
@@ -150,7 +171,7 @@ document.onkeypress = function (event) {
     document.querySelectorAll('#keyboard .k-key').forEach(element => {
         if (event.key == element.innerHTML) {
             element.classList.add('active');
-        }  else if (event.key == 32) {
+        }  else if (event.key == '' && element.innerHTML == 'Space') {
              element.classList.add('active');
         }
         // все слуаи
@@ -183,14 +204,57 @@ document.querySelectorAll('#keyboard .k-key').forEach(function(element) {
 
 
 document.addEventListener("keyup", function(event) {
-    console.log(event.keyCode);
-    if (event.keyCode == 17) {
+    // console.log(event.keyCode);
+    if (event.keyCode == 18 && shiftPressed) {
         langEn = !langEn;
         init(langEn ? keyboardEng : keyboardRu);
     }
-    
+    if(event.keyCode == 32) {
+    space.classList.remove('active');
+    }
+    if(event.keyCode == 16) {
+       shift.forEach((item) => {
+           item.classList.remove('active');
+       });
+    }
+    if(event.keyCode == 17) {
+       ctrl.forEach((item) => {
+           item.classList.remove('active');
+       });
+    }
+    if(event.keyCode == '91') {
+       win.classList.remove('active');
+    }
+    if(event.keyCode == '18') {
+        alt.forEach((item) => {
+            item.classList.remove('active');
+        });
+    }
+    if(event.keyCode == 13) {
+       enter.classList.remove('active');
+   }
+   if(event.keyCode == 46) {
+       del.classList.remove('active');
+   }
+   if(event.keyCode == 8) {
+       backspace.classList.remove('active');
+   }
+    if(event.keyCode == 38) {
+       up.classList.remove('active');
+   }
+   if(event.keyCode == 37) {
+       left.classList.remove('active');
+   }
+   if(event.keyCode == 40) {
+       down.classList.remove('active');
+   }
+   if(event.keyCode == 39) {
+       right.classList.remove('active');
+   }
+
   if(event.keyCode == 9) {
     input.value = input.value + '   ';
+    tab.classList.remove('active');
   }
   buttons.forEach((item) => {
     if (event.getModifierState("CapsLock")) {
@@ -210,8 +274,54 @@ input.addEventListener('keydown', (event) => {
 });
 
 document.addEventListener('keydown', (event) => {
-   if (event.key == 'shift') {
+
+   if (event.key == 'Control') {
        shiftPressed = true;
+   }
+   if(event.keyCode == 32) {
+       space.classList.add('active');
+   }
+   if(event.keyCode == 9) {
+       tab.classList.add('active');
+   }
+   if(event.keyCode == 16) {
+       shift.forEach((item) => {
+           item.classList.add('active');
+       });
+   }
+   if(event.keyCode == 17) {
+       ctrl.forEach((item) => {
+           item.classList.add('active');
+       });
+   }
+   if(event.keyCode == '91') {
+       win.classList.add('active');
+   }
+   if(event.keyCode == '18') {
+       alt.forEach((item) => {
+        item.classList.add('active');
+       });
+   }
+   if(event.keyCode == 13) {
+       enter.classList.add('active');
+   }
+   if(event.keyCode == 46) {
+       del.classList.add('active');
+   }
+   if(event.keyCode == 8) {
+       backspace.classList.add('active');
+   }
+   if(event.keyCode == 38) {
+       up.classList.add('active');
+   }
+   if(event.keyCode == 37) {
+       left.classList.add('active');
+   }
+   if(event.keyCode == 40) {
+       down.classList.add('active');
+   }
+   if(event.keyCode == 39) {
+       right.classList.add('active');
    }
 
 });
